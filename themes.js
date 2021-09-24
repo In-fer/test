ec61651aefa6c2f42811cdbe94981adb2b9d08ed
In-fer <90282790+In-fer@users.noreply.github.com>
2021-09-24 09:31:34 +0300

    Add files via upload

---
+++ themes.js
@@ -0,0 +1,10 @@
+let switchMode = document.getElementById("switchMode");
+switchMode.onclick = function() {
+    let theme =document.getElementById("theme");
+
+    if (theme.getAttribute("href")=="light.css") {
+        theme.href="dark.css"
+    }else{
+        theme.href="light.css"
+    }
+}
